d193a6a42e45ad594d52a1e577405395639dd989
Ahmad Jaber <ahmad.jaber010@yahoo.com>
2024-02-29 15:03:03 +0100

    remove .env contain

--- src/component/config.jsx
+++ src/component/config.jsx
@@ -9,14 +9,14 @@ import { getDatabase } from "firebase/database";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
   const firebaseConfig = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN ,
-    databaseURL: process.env.DATABASE_URL,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID,
-    measurementId: process.env.MEASUREMENT_ID
+    apiKey: "AIzaSyCIO7HC4BflPzh7AT0Ycfe_kKBNhqqDRmI",
+    authDomain: "morpiax-alpha.firebaseapp.com",
+    databaseURL: "https://morpiax-alpha-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "morpiax-alpha",
+    storageBucket: "morpiax-alpha.appspot.com",
+    messagingSenderId: "157426728474",
+    appId: "1:157426728474:web:ccd0eacc35e0501153dba3",
+    measurementId: "G-EDM8YEN9TG"
   };
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
